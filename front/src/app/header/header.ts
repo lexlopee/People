010cd20b890b71bec2e3f1fac9db0fe18a724d92
danie <danielfernandezdavila2005@gmail.com>
2026-03-18 12:58:19 +0100

    Creacion de la Cabecera

--- front/src/app/header/header.ts
+++ front/src/app/header/header.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css']
 })
 export class Header {}
